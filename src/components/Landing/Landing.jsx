53961868a652c6d3587309b3b745e9a4527ee6fb
Sarah Smith <sarahlib90@gmail.com>
2026-02-11 18:09:44 -0500

    refactor: set up landing page via wireframe using bootstrap

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,9 +1,40 @@
+import './Landing.css';
+import { Link } from 'react-router';
+import { Container, Row, Col, Button, Card } from 'react-bootstrap';
+
 const Landing = () => {
     return (
         <main>
-            <h1>Hello, you are on the landing page for visitors</h1>
-            <p>Sign up or sign in to see a secret dashboard</p>
+            {/* HERO SECTION */}
+            <section className='py-3 mb-5 border-bottom'>
+                <Container className='text-center py-5'>
+                    <Row className='justify-content-center'>
+                        <Col lg={8}>
+                            <img src="src/assets/devspace-landing.png" alt="DevSpace logo" className="landing-logo"/>
+                            <h1 className="display-3 fw-bold mb-3">{"<h1>"}DevSpace{"</h1>"}</h1>
+                                <h3 className="lead mb-4">{"<h3>"}A social network with zero merge conflicts.{"</h3>"}</h3>
+                                <div className='justify-content-center mb-4'>
+                                    <Button 
+                                        as={Link} 
+                                        to="/sign-in" 
+                                        variant="outline-dark" 
+                                        size="lg" 
+                                        className="rounded-pill px-5 fw-bold"
+                                    >
+                                        Connect
+                                    </Button>
+
+                                <div className='justify-content-center mt-4'>
+                                    <p className="text-center small text-muted">
+                                    Already a user? <Link to="/sign-in" className="text-primary text-decoration-none fw-bold">Sign in</Link>
+                                    </p>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
         </main>
-    )
-}
-export default Landing
+    );
+};
+export default Landing;
